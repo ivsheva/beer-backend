648104a2a94edc48bf03de8ceaddf17761663709
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-12-02 15:43:19 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors";
+
+const app = express()
+
+app.use(cors())
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log("App is listening on port", port)
+})
