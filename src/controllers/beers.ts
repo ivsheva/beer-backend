2c8a6bac82beab0189de86c3e7879a45037bd113
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-12-03 17:43:03 +0200

    Implement filtering

--- src/controllers/beers.ts
+++ src/controllers/beers.ts
@@ -4,15 +4,32 @@ import prisma from "../../prisma/client";
 export async function getBeers(req: Request, res: Response) {
   const filters = req.query;
 
+  const { minPrice, maxPrice, minRating } = filters;
+
+  // Remove used properties
+  delete filters.minPrice;
+  delete filters.maxPrice;
+  delete filters.minRating;
+
+  const where = {
+    price: {
+      gt: Number(minPrice) || undefined,
+      lt: Number(maxPrice) || undefined,
+    },
+    rating: {
+      gt: Number(minRating) || undefined,
+    },
+    ...filters,
+  };
+
   try {
     const products = await prisma.beer.findMany({
-      where: {
-        ...filters,
-      },
+      where,
     });
 
     res.send(products);
   } catch (error) {
+    console.log(error);
     res.status(500).send("Something went wrong. Check your request query.");
   }
 }
